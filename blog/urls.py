ff17bdd5ec10c49182fb4b934989fe7e4022664d
xudayfi <xudayfiibraahim8@gmail.com>
2024-05-09 06:11:24 +0300

    added some animations

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.home_page, name='home_page')
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
